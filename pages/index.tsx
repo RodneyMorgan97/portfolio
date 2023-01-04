04ddf7b8145cfea086b4b17d914f51557505ea93
Rodney Morgan <rtm36@1870.uakron.edu>
2023-01-04 11:10:18 -0500

    make flowers show pointer on hover and consolidate fontsize

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,7 +67,7 @@ export default function Home() {
               text={"Technical Mentor"}
               modalBackgroundColor={"#2A9D8F"}
             >
-              <div className="flex flex-col text-white p-4">
+              <div className="flex flex-col text-white p-4 md:text-2xl md:max-w-[50%]">
                 <p className="text-4xl pb-8">Technical Mentor</p>
                 <p className="pb-2">
                   {
@@ -92,7 +92,7 @@ export default function Home() {
               text={"Value Driven Solutions"}
               modalBackgroundColor={"#2A9D8F"}
             >
-              <div className="flex flex-col text-white p-4">
+              <div className="flex flex-col text-white p-4 md:text-2xl md:max-w-[50%]">
                 <p className="text-4xl pb-8">Value Driven Solutions</p>
                 <p className="pb-2">
                   {
@@ -139,19 +139,19 @@ export default function Home() {
               text={"Cross-Functional Communicator"}
               modalBackgroundColor={"#2A9D8F"}
             >
-              <div className="flex flex-col text-white p-4 md:max-w-[50%]">
+              <div className="flex flex-col text-white p-4 md:text-2xl md:max-w-[50%]">
                 <p className="text-4xl pb-8">Cross-Functional Communicator</p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "Throughout my time in the IT Department as an Integrations Programmer, it has been my responsibility to meet with business leaders across our organization to collaborate on technical solutions that accommodate business needs."
                   }
                 </p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "I plan to take this knowledge into my leadership to build strong working relationships with people from different backgrounds and skill sets, which can help me to better understand the needs and perspectives of others. This, in turn, can help me to make more informed decisions and find solutions to problems that benefit the entire organization. "
                   }
                 </p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "Additionally, being able to communicate effectively with people from different departments can help build trust and foster a positive work culture, which is crucial for any leader. "
                   }
@@ -172,14 +172,14 @@ export default function Home() {
               text={"Conflict Resolution"}
               modalBackgroundColor={"#E9C46A"}
             >
-              <div className="flex flex-col text-white p-4 md:max-w-[50%]">
+              <div className="flex flex-col text-white p-4 md:text-2xl md:max-w-[50%]">
                 <p className="text-4xl pb-8">Conflict Resolution</p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "I believe that conflict resolution is an important skill for any leader to possess, as it allows them to effectively address and resolve conflicts that may arise within their team or organization.  As a leader, it is my responsibility to create a positive and productive work environment, and being able to effectively resolve conflicts is crucial in achieving this goal. "
                   }
                 </p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "I feel like I haven't been challenged with much conflict in my leadership experience (thankfully) and will not be fully prepared to handle it appropriately when it inevitably occurs. I believe that participating in the MIT leadership training program will give me the tools and knowledge I need to better understand and address conflicts in the future."
                   }
@@ -191,9 +191,9 @@ export default function Home() {
               text={"Learning From Experience"}
               modalBackgroundColor={"#E9C46A"}
             >
-              <div className="flex flex-col text-white p-4 md:max-w-[50%]">
+              <div className="flex flex-col text-white p-4 md:text-2xl md:max-w-[50%]">
                 <p className="text-4xl pb-8">Learning From Experience</p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "In the MIT leadership training program, I will have the chance to learn from experienced leaders who have a wealth of knowledge and experience to share. I believe that learning from these individuals will be a valuable opportunity for me to gain insights and perspectives that I may not have considered before. I am eager to learn from their experiences and to apply what I learn to my own leadership journey. I believe that this type of learning will help me to become a more effective and respected leader within my organization."
                   }
@@ -205,9 +205,9 @@ export default function Home() {
               text={"Expanding My Network"}
               modalBackgroundColor={"#E9C46A"}
             >
-              <div className="flex flex-col text-white p-4 md:max-w-[50%]">
+              <div className="flex flex-col text-white p-4 md:text-2xl md:max-w-[50%]">
                 <p className="text-4xl pb-8">Expanding My Network</p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "I believe that expanding my network of mentors and connecting with other program attendees is an important aspect of my leadership development. In the MIT leadership training program, I will have the opportunity to learn from experienced mentors, as well as connect with other program attendees who are also aspiring to become leaders. I believe that having these connections will be a valuable resource for me as I continue to grow and develop my leadership skills. Additionally, I believe that connecting with other program attendees will be beneficial because I will inevitably be working with these individuals in the future, and building strong relationships with them now will help me to be a more effective leader when it's time for the next generation of leaders to step-in. "
                   }
@@ -228,24 +228,24 @@ export default function Home() {
               text={"Company-first Mentality"}
               modalBackgroundColor={"#E76F51"}
             >
-              <div className="flex flex-col text-white p-4 md:max-w-[50%]">
+              <div className="flex flex-col text-white p-4 md:max-w-[50%] md:text-2xl">
                 <p className="text-4xl pb-8">Company-first Mentality</p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "I believe that it's in the best interest of MP Biomedicals to invest their time and resources into me because I have the company's best interest in mind when performing my daily tasks."
                   }
                 </p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "One way I have demonstrated this dedication is by striving to do things the right way, even when it isn't the easiest option. When programming, I am sure to write readable documentation so that other programmers (current or future) will be able to understand my thought process throughout the development lifecycle. Also, I'll take the time to make my code scalable and easily expandable so it can be reused whenever the company's needs change."
                   }
                 </p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "Another way I demonstrate a company-first mentality is my contribution to the IT Team's yearly goals. I get the opportunity to look at the state of our company and determine the future-projects that will have the most immediate impact on my coworkers. Sometimes this requires major changes and collaborative projects, but other times there are some small, easy wins that I get to help MP Biomedicals reach."
                   }
                 </p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "Finally, a third way I show that I have the company's best interest in mind through my daily work is by treating those around me with respect. While this one isn't technical, I think that it goes a long way to be kind to everyone I interact with. My intention is to make everyone's life easier, and refrain from being the bottleneck on our collective path to success. "
                   }
@@ -257,28 +257,27 @@ export default function Home() {
               text={"Eager to Grow"}
               modalBackgroundColor={"#E76F51"}
             >
-              <div className="flex flex-col text-white p-4 md:max-w-[50%]">
+              <div className="flex flex-col text-white p-4 md:text-2xl md:max-w-[50%]">
                 <p className="text-4xl pb-8">Eager to Grow</p>
                 <p className="text-3xl pb-2">Growth Mindset</p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "I am someone who believes that I can ALWAYS improve and grow, whether it be in leadership or technical ability. I view challenges and setbacks as opportunities to learn and grow, rather than as failures. "
                   }
                 </p>
                 <p className="text-3xl pb-2">Proactive behavior</p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "I see myself as someone who is proactive in seeking out opportunities to learn and improve. I believe that I take initiative to seek out resources, ask for feedback, and volunteer for new tasks and projects."
                   }
                 </p>
                 <p className="text-3xl pb-2">Desire to learn</p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "I am always looking to learn more. In fact, one of the reasons I built this website for my application is because I wanted to try out a new web hosting platform I haven't used before. This is also my first time purchasing a domain and making it available on the web. Honestly, it would have been easier to make a Powerpoint, but I wanted to challenge myself to try something"
                   }
                   <b>{" different"}</b>
                 </p>
-                <p className="pb-4 md:text-2xl">{""}</p>
               </div>
             </FlowerWithModal>
             <FlowerWithModal
@@ -286,24 +285,24 @@ export default function Home() {
               text={"Big Ideas"}
               modalBackgroundColor={"#E76F51"}
             >
-              <div className="flex flex-col text-white p-4 md:max-w-[50%]">
+              <div className="flex flex-col text-white p-4 md:max-w-[50%] md:text-2xl">
                 <p className="text-4xl pb-8">Big Ideas</p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "MP Biomedicals has consistently deomnstrated greatness by overcoming the odds, crushing projections, and contributing to leading scientific advances."
                   }
                 </p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "There are many ways that we can continue to provide these opportunities, but as a Software Developer my mind is drawn towards increasing our potential with programming."
                   }
                 </p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "I would like to make it even easier to get our products in the hands of our customers. Some ways we can accomplish this would be through enhanced data visualization, increasing our marketing team's insights, displaying our products through more vendor websites, and allowing us to make quick iterations mpbio.com"
                   }
                 </p>
-                <p className="pb-4 md:text-2xl">
+                <p className="pb-4">
                   {
                     "While this sounds exciting, our opportunities are limited with a small team of developers. With my leadership skills growing, I believe I'll be in a position to leverage our development partners and internal development talent to begin accomplishing these goals in the near future."
                   }
